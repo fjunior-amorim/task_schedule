4a4aa48663c6123400631c635818d228243a7e93
FABIO JUNIO <86170296+fjunior-amorim@users.noreply.github.com>
2023-01-15 03:05:30 -0300

    updaded

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'calendario',
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.scss']
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit {
 
+  months: string[] = [
+    "Janeiro","Fevereiro","Março","April",
+    "Maio","Junho","Julho","Augosto",
+    "Setembro","Outobro","Novembro","Decembro"
+  ];
+
+  date: Date = new Date();
+  day: string = this.date.toDateString();
+  month: string = '';
+  weeksDays: string[] = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEc', 'SAB'];
+
+  
+  
+  lastDay = new Date(this.date.getFullYear(), 
+  this.date.getMonth() + 1, 0
+  ).getDate();
+
+  prevLastDay = new Date(this.date.getFullYear(),
+   this.date.getMonth(), 0
+  ).getDate();
+  firstDayIndex = this.date.getDay();
+
+  lastDayIndex = new Date(this.date.getFullYear(), 
+  this.date.getMonth() + 1, 0
+  ).getDay();
+  nextDaysMonth = 7 - this.lastDayIndex - 1;
+
+  monthDays: number[] = [];
+  nextDays: number[] = [];
+  firstDays: number[] = [];
+
+  constructor() {  }
+
+  getMonthDays() {
+    for (let i = 1; i <= this.lastDay; i++) {
+      this.monthDays.push(i);
+    }
+
+    for (let i = this.firstDayIndex; i > 0; i--) {
+      this.firstDays.push(this.prevLastDay - i);
+    }
+
+    for (let i = 1; i <= this.nextDaysMonth; i++) {
+      this.nextDays.push(i);
+    }
+
+  }
+  cont: number = this.date.getMonth();
+  onClickNext() {
+    if(this.cont < 0) {
+      this.cont = 11;
+      this.getMonths(this.cont);
+    }else if(this.cont === 12) {
+      this.cont = 0;
+      this.getMonths(this.cont);
+    }else {
+      this.getMonths(this.cont++);
+      console.log(this.cont)
+    }
+  }
+  onClickPrev() {
+    if(this.cont < 0 || this.cont === -1) {
+      this.cont = 11;
+      this.getMonths(this.cont);
+    }else if(this.cont === 12) {
+      this.cont = 0;
+      this.getMonths(this.cont);
+    }else {
+      this.getMonths(this.cont--);
+      console.log(this.cont)
+    }
+  }
+
+  //metodo para pegar o index do mes e passar para o array dos messes
+  getMonths(mes: number) {
+    const d = new Date();
+    //o mes e setado de acordo com o valor recebido do parametro
+    d.setMonth(mes);
+    this.month = this.months[d.getMonth()];
+    this.months[d.getMonth()];
+  }
+
+  ngOnInit() {
+    console.log(this.prevLastDay)
+    console.log(this.firstDayIndex)
+    this.getMonthDays();
+    this.getMonths(this.cont);
+    
+  }
 }
--- src/app/components/relogio/relogio.component.ts
+++ src/app/components/relogio/relogio.component.ts
@@ -1,78 +1,101 @@
-import { Component,  OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'relogio',
   templateUrl: './relogio.component.html',
-  styleUrls: ['./relogio.component.scss']
+  styleUrls: [ 
+    './relogio.component.scss' 
+  ],
 })
-export class RelogioComponent implements OnInit{
-
-  horas: string = '';
-  dia: string = '';
-  data: string = '';
-  ano: string = '';
+export class RelogioComponent implements OnInit {
+  data: string = ''
+  ano: string = ''
+  dia: string = ''
+  horas: string = ''
+  minutos: string = ''
 
   months: string[] = [
-    "Janeiro","Fevereiro","Março","April",
-    "Maio","Junho","Julho","Augosto",
-    "Setembro","Outobro","Novembro","Decembro"
-  ];
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'April',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Augosto',
+    'Setembro',
+    'Outobro',
+    'Novembro',
+    'Decembro',
+  ]
 
   days: string[] = [
-    "Domingo", "Segunda", "Terça", "Quarta",
-    "Quinta", "Sexta", "Sábado"
-  ];
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ]
 
-
-  getHours() {
-    const h = new Date().getHours();
-    const m = new Date().getMinutes();
-    if(h < 10 ) {
-      this.horas = `0${h}:${m}`;
-      console.log(this.horas)
-    }else{
-      this.horas = `${h}:${m}`;
-      console.log(this.horas)
-    }
-  }
-  getData() {
-    const dia = new Date().getDate();
-    const mes = this.getMes();
+  getData(): any {
+    const dia = new Date().getDate()
+    const mes = this.getMes()
     return `${dia} de ${mes}`
   }
 
   getDia() {
-    const d = new Date();
-    let day = this.days[d.getDay()];
-    return day;
+    const d = new Date()
+    let day = this.days[d.getDay()]
+    return day
   }
 
   getMes() {
-    const d = new Date();
-    let month = this.months[d.getMonth()];
-    return month;
+    const d = new Date()
+    let month = this.months[d.getMonth()]
+    return month
   }
 
   getYear() {
-    const d = new Date();
-    let year = d.getFullYear();
-    return `${year}`;
+    const d = new Date()
+    let year = d.getFullYear()
+    return `${year}`
   }
 
   updateHours() {
-    let contador = interval(1000);
-    contador.subscribe(n => {
-      this.getHours();
+    let contador = interval(1000)
+    contador.subscribe((n) => {
+      this.getHours()
     })
   }
 
-  ngOnInit () {
-    //this.horas = this.getHours();
+  getHours() {
+    const h = new Date().getHours()
+    const m = new Date().getMinutes()
+
+    if (h < 10) {
+      if(m < 10) {
+        this.minutos = `0${m}`
+      }else {
+        this.horas = `0${h}`
+        this.minutos = `${m}`
+      }
+    } else {
+      if(m < 10) {
+        this.minutos = `0${m}`
+      }else {
+        this.horas = `${h}`
+        this.minutos = `${m}`
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.updateHours()
     this.ano = this.getYear()
     this.dia = this.getDia()
     this.data = this.getData()
-
-    this.updateHours();
   }
 }
